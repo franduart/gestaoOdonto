ce77c207fce9bb5223163a8e1daca8dbf75d32eb
Francieli Duarte <120760196+franduart@users.noreply.github.com>
2025-12-15 23:28:30 -0300

    Update Hero.tsx

--- src/app/gestaoOdonto/components/hero/Hero.tsx
+++ src/app/gestaoOdonto/components/hero/Hero.tsx
@@ -18,6 +18,12 @@ export function Hero() {
           <strong className="text-[#51A2FF] font-medium text-[1.2rem]">
             Automatize sua rotina, aumente sua captação de leads e ofereça uma experiência moderna para seus pacientes.
           </strong>
+
+          <h4 className="text-zinc-700">Clique no botão abaixo e comece agora.</h4>
+          
+          <p className="font-medium text-2xl text-center">15 dias grátis <br /> cancele quando quiser</p>
+          <a href='https://gestao-odonto-cyan.vercel.app/' className="bg-[#2F5FFF] p-1.5 font-medium rounded-2xl text-center text-white text-[1.2rem] shadow-2xl" target="blank">Testar agora mesmo </a>
+          </div>
         </div>
       </div>
 
